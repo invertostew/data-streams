8e29e732bbcf3c4fac8b663f01a9ac948cff7b6e
Stuart Green <github.47a8z@aleeas.com>
2022-02-13 13:47:18 +0000

    filter habitable planets

--- index.js
+++ index.js
@@ -2,7 +2,13 @@ const fs = require('fs');
 
 const { parse } = require('csv-parse');
 
-const results = [];
+const habitablePlanets = [];
+
+function isHabitablePlanet(planet) {
+  return planet['koi_disposition'] === 'CONFIRMED'
+    && planet['koi_insol'] > 0.36 && planet['koi_insol'] < 1.11
+    && planet['koi_prad'] < 1.6;
+}
 
 fs.createReadStream('kepler_data.csv')
   .pipe(parse({
@@ -10,12 +16,13 @@ fs.createReadStream('kepler_data.csv')
     columns: true
   }))
   .on('data', data => {
-    results.push(data);
+    if (isHabitablePlanet(data)) {
+      habitablePlanets.push(data);
+    }
   })
   .on('error', error => {
     console.log(error);
   })
   .on('end', () => {
-    console.log(results);
-    console.log('Done processing');
+    console.log(`${habitablePlanets.length} habitable planets found!`);
   });
