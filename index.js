c01b4448383fed95c0048dee07b53528a4cb9304
Stuart Green <github.47a8z@aleeas.com>
2022-02-13 13:37:33 +0000

    parse bytes into js objects

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
+const fs = require('fs');
+
 const { parse } = require('csv-parse');
+
+const results = [];
+
+fs.createReadStream('kepler_data.csv')
+  .pipe(parse({
+    comment: '#',
+    columns: true
+  }))
+  .on('data', data => {
+    results.push(data);
+  })
+  .on('error', error => {
+    console.log(error);
+  })
+  .on('end', () => {
+    console.log(results);
+    console.log('Done processing');
+  });
